80b5d073c360ec747c5a84b8409931f88b23ccd0
olivo2b <rougailles@hotmail.com>
2022-09-06 14:57:53 +0200

    nico

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String args []){
-        System.out.println ("hello from Main");
+        System.out.println ("hello from Main git002");
         Pangolin p = new Pangolin ("toto", 5);
         p.crier();
     }
